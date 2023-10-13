508337725bc08543eb23004a445d58761571b32f
KhanyieShass <sharonkhanyisile892@gmail.com>
2023-10-13 14:45:18 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,24 +1,22 @@
-#!/usr/bin/python3
+#include "lists.h"
 
-def safe_print_list(my_list=[], x=0):
-    """
-    Prints x elements of a list.
-    All elements must be printed on the same line followed by a new line.
-    You have to use try: / except:
-    You are not allowed to import any module
-    You are not allowed to use len()
-        Args:
-            my_list: list
-            x: number of elements to print.
-        Returns: the real number of elements printed.
-    """
-    count = 0
-    for item in range(x):
-        try:
-            print(my_list[item], end='')
-        except IndexError:
-            break
-        else:
-            count += 1
-    print()
-    return coun
+/**
+ * print_dlistint - function that prints all the elemts of a doubly linked list
+ * @h: poiner to the head of the list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	/* while we have a pointer to the list */
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
